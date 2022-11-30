da85ab43583c307dd0d315f95ed63eb689279694
ZedZeroth <jhaywood@protonmail.com>
2022-11-30 18:24:59 +0000

    CommandInformerTest finished

--- tests/Unit/app/Console/Commands/CommandInformerTest.php
+++ tests/Unit/app/Console/Commands/CommandInformerTest.php
@@ -31,7 +31,7 @@ test('GIVEN a correctly mocked Command
         ->shouldReceive('info')->with('')->andReturn()
         ->getMock();
 
-    // Inject the mock into a new CommandInformer and call run()
+    // Inject the mock into a new CommandInformer's run() method
     expect(
         (new CommandInformer())->run($commandMock)
     )->toBeNull();
@@ -47,7 +47,7 @@ test('GIVEN a mocked Command with "command" argument "t:t"
         ->shouldReceive('argument')->with('command')->andReturn('t:t')
         ->getMock();
 
-    // Inject the mock into a new CommandInformer and call run()
+    // Inject the mock into a new CommandInformer's run() method
     expect(
         (new CommandInformer())->run($commandMock)
     )->toBeNull();
@@ -65,15 +65,73 @@ test('GIVEN a mocked Command with "source" argument "test"
         ->shouldReceive('argument')->with('source')->andReturn('test')
         ->getMock();
 
-    // Inject the mock into a new CommandInformer and call run()
+    // Inject the mock into a new CommandInformer's run() method
     expect(
         (new CommandInformer())->run($commandMock)
     )->toBeNull();
 })
 ->expectException(\App\Console\Commands\CommandValidationException::class);
 
+/**
+ * Testing the output() method
+ */
 
+test('GIVEN a correctly mocked Command
+    WHEN calling ouput("[📟] test:test")
+    THEN it returns null
+    ', function () {
+    // Mock a Command to return a fake request response array
+    $commandMock = mock(Illuminate\Console\Command::class)
+        ->shouldReceive('argument')->with('command')->andReturn('test:test')
+        ->shouldReceive('argument')->with('source')->andReturn('cli')
+        ->shouldReceive('info')->with('[📟] test:test')->andReturn()
+        ->shouldReceive('info')->with('---------------------------------')->andReturn()
+        ->shouldReceive('argument')->with()->andReturn([])
+        ->shouldReceive('info')->with('... Running "test:test"')->andReturn()
+        ->shouldReceive('runThisCommand')->with()->andReturn()
+        ->shouldReceive('info')->with('... 0ms DONE')->andReturn()
+        ->shouldReceive('info')->with('No new models created.')->andReturn()
+        ->shouldReceive('info')->with('')->andReturn()
+        ->getMock();
 
+    // Inject the mock into a new CommandInformer's run() method
+    $injectedCommandInformer = new CommandInformer();
+    $injectedCommandInformer->run($commandMock);
+
+    // Call output('test') on the CommandInformer
+    expect(
+        $injectedCommandInformer->output('[📟] test:test')
+    )->toBeNull();
+});
+
+test('GIVEN a string of length 1000
+    WHEN calling ouput()
+    THEN a StringValidationException is thrown
+    ', function () {
+    // Mock a Command to return a fake request response array
+    $commandMock = mock(Illuminate\Console\Command::class)
+        ->shouldReceive('argument')->with('command')->andReturn('test:test')
+        ->shouldReceive('argument')->with('source')->andReturn('cli')
+        ->shouldReceive('info')->with('[📟] test:test')->andReturn()
+        ->shouldReceive('info')->with('---------------------------------')->andReturn()
+        ->shouldReceive('argument')->with()->andReturn([])
+        ->shouldReceive('info')->with('... Running "test:test"')->andReturn()
+        ->shouldReceive('runThisCommand')->with()->andReturn()
+        ->shouldReceive('info')->with('... 0ms DONE')->andReturn()
+        ->shouldReceive('info')->with('No new models created.')->andReturn()
+        ->shouldReceive('info')->with('')->andReturn()
+        ->getMock();
+
+    // Inject the mock into a new CommandInformer's run() method
+    $injectedCommandInformer = new CommandInformer();
+    $injectedCommandInformer->run($commandMock);
+
+    // Call output('test') on the CommandInformer
+    expect(
+        $injectedCommandInformer->output(str_pad('', pow(10, 3)))
+    )->toBeNull();
+})
+->expectException(\App\Http\Controllers\MultiDomain\Validators\StringValidationException::class);
 
 
 
